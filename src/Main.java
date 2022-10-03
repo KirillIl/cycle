f683a99395e73f11bdb760181d7145c3004b4705
Admin <ilinskiy_kirill@mail.ru>
2022-10-03 16:32:58 +0300

    home work1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Задание 1");
+       for (int i = 1; i <= 10; i++){
+           System.out.println( "Итерация цикла " + i);
+       }
+       System.out.println( "Задание 2");
+       for ( int i= 10; i >= 1; i--){
+           System.out.println("Интерация цикала " + i);
+       }
+       System.out.println("Задание 3");
+       for ( int i = 0; i <= 17; i+=2) {
+            System.out.println(" Четные числа " + i);
+       }
+        System.out.println( " Задание 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println( i);
+            
+        }
+
+
+    }
+}
